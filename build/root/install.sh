2615268bd27b8c6ce0027833cedf4f1e928ccc54
Scottst <scott@stevensononthe.net>
2025-02-17 13:20:08 -0500

    Update install.sh
    Update to the newest rtorrent and rutorrent.  Removed irssi since its not compatible with the newest rutorrent.
    
    Note: This is a hack job.  Use at your own rick.

--- build/root/install.sh
+++ build/root/install.sh
@@ -6,42 +6,87 @@ set -e
 # release tag name from build arg, stripped of build ver using string manipulation
 release_tag_name="${1//-[0-9][0-9]/}"
 
-# note do NOT download build scripts - inherited from int script with envvars common defined
-
-# detect image arch
-####
-
-OS_ARCH=$(cat /etc/os-release | grep -P -o -m 1 "(?=^ID\=).*" | grep -P -o -m 1 "[a-z]+$")
-if [[ ! -z "${OS_ARCH}" ]]; then
-	if [[ "${OS_ARCH}" == "arch" ]]; then
-		OS_ARCH="x86-64"
-	else
-		OS_ARCH="aarch64"
-	fi
-	echo "[info] OS_ARCH defined as '${OS_ARCH}'"
-else
-	echo "[warn] Unable to identify OS_ARCH, defaulting to 'x86-64'"
-	OS_ARCH="x86-64"
-fi
-
 # custom
 ####
 
-libtorrentps_package_name="libtorrent-ps.tar.xz"
+mkdir /home/nobody/bin/
 
-# download compiled libtorrent-ps (used by rtorrent-ps)
-rcurl.sh -o "/tmp/${libtorrentps_package_name}" "https://github.com/rakshasa/rtorrent/releases/download/v0.15.1/libtorrent-0.15.1.tar.gz"
 
-# install libtorrent-ps
-pacman -U "/tmp/${libtorrentps_package_name}" --noconfirm
+# pacman packages
+####
 
-rtorrentps_package_name="rtorrent-ps.tar.xz"
+# define pacman packages
+pacman_packages="svn git nginx php-fpm rsync openssl tmux mediainfo php-geoip zip libx264 libvpx xmlrpc-c sox python-pip"
 
-# download compiled rtorrent-ps (cannot compile during docker build)
-rcurl.sh -o "/tmp/${rtorrentps_package_name}" "https://github.com/rakshasa/rtorrent/releases/download/v0.15.1/rtorrent-0.15.1.tar.gz"
+# install compiled packages using pacman
+if [[ ! -z "${pacman_packages}" ]]; then
+	pacman -S --needed $pacman_packages --noconfirm
+fi
 
-# install rtorrent-ps
-pacman -U "/tmp/${rtorrentps_package_name}" --noconfirm
+
+# rTorrent Install
+#
+#
+
+# Installing UDNS
+#
+cd /tmp
+git clone https://github.com/shadowsocks/libudns;
+cd libudns;
+./autogen.sh;
+./configure --prefix=/usr;
+make -j$(nproc) CFLAGS="-O3 -fPIC";
+make -j$(nproc) install;
+cd /tmp;
+rm -R libudns;
+
+# Installing xmlrpc-c
+#
+
+#advanced - v1.63.xx (recommended for home users)
+svn checkout svn://svn.code.sf.net/p/xmlrpc-c/code/advanced xmlrpc-c_advanced;
+cd xmlrpc-c_advanced;
+
+./configure --prefix=/usr --disable-cplusplus --disable-wininet-client --disable-libwww-client --disable-abyss-server --disable-cgi-server;
+make -j$(nproc) CFLAGS="-O3";
+make -j$(nproc) install;
+cd /tmp;
+rm -R xmlrpc-c_advanced;
+
+# Installing libtorrent
+#
+# We strongly advise to configure with aligned memory access (--enable-aligned) to avoid critical stability issues.
+#
+#We recommend to configure with instrumentation disabled (--disable-instrumentation) to improve performance. This feature has been fixed since version 5.3.
+#
+#We recommend to configure with UDNS enabled (--enable-udns) to improve stability and performance of UDP trackers. This feature is stable as of version 6.0.
+#
+# We do not recommend using file preload. It's better to leave this decision to the Linux Kernel. 
+# You can reduce the overhead of the peer connection protocol, by disabling it entirely at compile time with (--enable-hosted-mode). 
+# If pieces.preload.type is changed from ruTorrent or .rtorrent.rc it will accept the value and ignore it for 100% backwards compatibility.
+
+git clone https://github.com/stickz/rtorrent stickz-rtorrent;
+cd stickz-rtorrent/libtorrent;
+./autogen.sh;\
+./configure --prefix=/usr --enable-aligned --enable-hosted-mode --disable-instrumentation --enable-udns;\
+make -j$(nproc) CXXFLAGS="-O3";\
+make -j$(nproc) install;
+
+
+# Installing rTorrent
+#
+
+cd ../rtorrent;\
+./autogen.sh;\
+./configure --prefix=/usr --with-xmlrpc-c --with-ncurses;\
+make -j$(nproc) CXXFLAGS="-O3";\
+make -j$(nproc) install;
+cd /tmp;
+rm -R stickz-rtorrent;
+
+cd /;
+
+# Done rTorrent install
 
 # set tmux to use 256 colors (required by rtorrent-ps)
 echo 'set -g default-terminal "screen-256color"' > /home/nobody/.tmux.conf
@@ -49,81 +94,41 @@ echo 'set -g default-terminal "screen-256color"' > /home/nobody/.tmux.conf
 ffmpeg_package_name="ffmpeg-release-static.tar.xz"
 
 # download statically linked ffmpeg (used by rutorrent screenshots plugin)
-rcurl.sh -o "/tmp/${ffmpeg_package_name}" "https://github.com/binhex/packages/raw/master/static/${OS_ARCH}/ffmpeg/johnvansickle/${ffmpeg_package_name}"
+rcurl.sh -o "/tmp/${ffmpeg_package_name}" "https://dev.scne.xyz/packages/${ffmpeg_package_name}"
 
 # unpack and move binaries
 mkdir -p "/tmp/unpack" && tar -xvf "/tmp/${ffmpeg_package_name}" -C "/tmp/unpack"
 mv /tmp/unpack/ffmpeg*/ff* "/usr/bin/"
 
-# pacman packages
-####
-
-# define pacman packages
-pacman_packages="git nginx php-fpm rsync openssl tmux mediainfo php-geoip zip libx264 libvpx xmlrpc-c sox python-pip"
-
-# install compiled packages using pacman
-if [[ ! -z "${pacman_packages}" ]]; then
-	pacman -S --needed $pacman_packages --noconfirm
-fi
-
-# aur packages
-####
-
-# define aur packages
-aur_packages="autodl-irssi-community"
-
-# call aur install script (arch user repo) - note true required due to autodl-irssi error during install
-source aur.sh
 
 # custom
 ####
 
-package_name="python2.tar.zst"
+# rutorrent
+# v5.1.5-hotfix
+rutorrent_package_name="v5.1.5.zip"
 
-# download compiled python2 (removed from AOR)
-rcurl.sh -o "/tmp/${package_name}" "https://github.com/binhex/packages/raw/master/compiled/${OS_ARCH}/${package_name}"
-
-# install python2
-pacman -U "/tmp/${package_name}" --noconfirm
-
-# github release - rutorrent
-####
-
-# download rutorrent
-github.sh --install-path "/usr/share/webapps/rutorrent" --github-owner "Novik" --github-repo "ruTorrent" --query-type "branch" --download-branch "master"
+rcurl.sh -o "/tmp/${rutorrent_package_name}" "https://github.com/Novik/ruTorrent/archive/refs/tags/${rutorrent_package_name}"
+unzip "/tmp/${rutorrent_package_name}" -d /tmp/
+mkdir -p /usr/share/webapps/rutorrent
+mv /tmp/ruTorrent-*/* /usr/share/webapps/rutorrent
 
 # rutorrent plugin cloudflare requires python module 'cloudscraper', use pip to install (python-pip = python 3.x)
-pip install --ignore-installed cloudscraper
+pip install --break-system-packages  --ignore-installed cloudscraper
 
-# github release - pyrocore
+# Pyrocore - new package pyrosimple
 ####
-
-# download pyrocore tools for rtorrent-ps
-git clone "https://github.com/pyroscope/pyrocore.git" "/opt/pyrocore" && cd "/opt/pyrocore"
-
-# manually create folder, used to create symlinks to pyrocore binaries
-mkdir -p "/home/nobody/bin"
-
-# run install script which updates to github head and then installs python modules using pip
-./update-to-head.sh "/usr/bin/python2"
-
-# install additional python modules using pip (pip laid on disk as part of pyrocore) - required
-# for pycore torque utility
-# note we also require gcc to compile python module psutil
-pacman -S --needed gcc --noconfirm
-/opt/pyrocore/bin/pip install --ignore-installed -r "/opt/pyrocore/requirements-torque.txt"
-
-# github master branch - autodl-irssi
-####
-
-# download autodl-irssi community plugin
-github.sh --install-path "/usr/share/webapps/rutorrent/plugins/autodl-irssi" --github-owner "autodl-community" --github-repo "autodl-rutorrent"
+pip install --break-system-packages pyrosimple
 
 # download htpasswd (problems with apache-tools and openssl 1.1.x)
-rcurl.sh -o /tmp/htpasswd.tar.gz "https://github.com/binhex/packages/raw/master/compiled/${OS_ARCH}/htpasswd.tar.gz"
+rcurl.sh -o /tmp/htpasswd.tar.gz "https://dev.scne.xyz/packages/htpasswd.tar.gz"
 
 # extract compiled version of htpasswd
 tar -xvf /tmp/htpasswd.tar.gz -C /
+
+# Dumptorrent - Plugin Requirement
+github.sh --install-path "/usr/bin" --github-owner "TheGoblinHero" --github-repo "dumptorrent" --compile-src make
+
 
 # config - nginx
 ####
@@ -133,6 +138,7 @@ tar -xvf /tmp/htpasswd.tar.gz -C /
 # see here for details:-
 # https://unix.stackexchange.com/questions/233275/hard-link-creation-permissions
 chmod 777 /usr/bin/nginx
+
 
 # config - php
 ####
@@ -160,8 +166,9 @@ sed -i -e "s~.*post_max_size\s\=\s.*~post_max_size = 25M~g" "${php_ini}"
 # configure php with additional php-geoip module
 sed -i -e "/.*extension=gd/a extension=geoip" "${php_ini}"
 
+
 # configure php to enable sockets module (used for autodl-irssi plugin)
-sed -i -e "s~.*extension=sockets~extension=sockets~g" "${php_ini}"
+#sed -i -e "s~.*extension=sockets~extension=sockets~g" "${php_ini}"
 
 # configure php-fpm to use tcp/ip connection for listener
 php_fpm_ini="/etc/php/php-fpm.conf"
@@ -225,44 +232,12 @@ sed -i -e "s~\$partitionDirectory \= \&\$topDirectory\;~\$partitionDirectory \= 
 sed -i -e 's~test -x.*&&\s~~g' '/usr/share/webapps/rutorrent/php/test.sh'
 # /delme
 
-# config - autodl-irssi
-####
-
-# copy default configuration file
-cp "/usr/share/webapps/rutorrent/plugins/autodl-irssi/_conf.php" "${rutorrent_plugins_path}/autodl-irssi/conf.php"
-
-# set config for autodl-irssi plugin
-sed -i -e 's~^$autodlPort.*~$autodlPort = 12345;~g' "${rutorrent_plugins_path}/autodl-irssi/conf.php"
-sed -i -e 's~^$autodlPassword.*~$autodlPassword = "autodl-irssi";~g' "${rutorrent_plugins_path}/autodl-irssi/conf.php"
-
-# set config for autodl (must match port and password specified in /usr/share/webapps/rutorrent/plugins/autodl-irssi/conf.php)
-mkdir -p /home/nobody/.autodl
-cat <<'EOF' > /home/nobody/.autodl/autodl.cfg.bak
-[options]
-gui-server-port = 12345
-gui-server-password = autodl-irssi
-EOF
-
-# add in option to enable/disable autodl-irssi plugin depending on env var
-# ENABLE_AUTODL_IRSSI value which is set when /home/nobody/irssi.sh runs
-cat <<'EOF' >> "${rutorrent_config_path}/plugins.ini"
-
-[autodl-irssi]
-enabled = no
-EOF
-
-# create symlink to autodl script so it auto runs when irssi (irc chat client) starts
-mkdir -p /home/nobody/.irssi/scripts/autorun
-cd /home/nobody/.irssi/scripts
-ln -s /usr/share/autodl-irssi/AutodlIrssi/ .
-cd /home/nobody/.irssi/scripts/autorun
-ln -s /usr/share/autodl-irssi/autodl-irssi.pl .
 
 # container perms
 ####
 
 # define comma separated list of paths
-install_paths="/usr/share/webapps,/usr/share/nginx/html,/etc/nginx,/etc/php,/run/php-fpm,/var/lib/nginx,/var/log/nginx,/etc/privoxy,/home/nobody,/usr/share/autodl-irssi"
+install_paths="/usr/share/webapps,/usr/share/nginx/html,/etc/nginx,/etc/php,/run/php-fpm,/var/lib/nginx,/var/log/nginx,/etc/privoxy,/home/nobody"
 
 # split comma separated string into list for install paths
 IFS=',' read -ra install_paths_list <<< "${install_paths}"
@@ -317,14 +292,6 @@ rm /tmp/permissions_heredoc
 ####
 
 cat <<'EOF' > /tmp/envvars_heredoc
-
-export ENABLE_AUTODL_IRSSI=$(echo "${ENABLE_AUTODL_IRSSI}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
-if [[ ! -z "${ENABLE_AUTODL_IRSSI}" ]]; then
-	echo "[info] ENABLE_AUTODL_IRSSI defined as '${ENABLE_AUTODL_IRSSI}'" | ts '%Y-%m-%d %H:%M:%.S'
-else
-	echo "[warn] ENABLE_AUTODL_IRSSI not defined (via -e ENABLE_AUTODL_IRSSI), defaulting to 'no'" | ts '%Y-%m-%d %H:%M:%.S'
-	export ENABLE_AUTODL_IRSSI="no"
-fi
 
 export ENABLE_RPC2=$(echo "${ENABLE_RPC2}" | sed -e 's~^[ \t]*~~;s~[ \t]*$~~')
 if [[ ! -z "${ENABLE_RPC2}" ]]; then
